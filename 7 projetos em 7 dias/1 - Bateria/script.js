0ad04441a35eaf4ffcb35f25731d466cf2abba20
Ana Beatriz Silva Araújo <anabeatrizaraujo0207@gmail.com>
2024-10-13 15:28:53 -0300

    bateria

---
+++ 7 projetos em 7 dias/1 - Bateria/script.js
@@ -0,0 +1,26 @@
+document.body.addEventListener('keyup', (event) => {
+    playSound(event.code.toLowerCase())
+
+})
+
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.getElementById('input')
+})
+
+function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`)
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+
+    if(audioElement) {
+        audioElement.currentTime = 0
+        audioElement.play()
+    }
+
+    if(keyElement) {
+        keyElement.classList.add('active')
+
+        setTimeout( () => {
+            keyElement.classList.remove('active')
+        }, 300)
+    }
+}
