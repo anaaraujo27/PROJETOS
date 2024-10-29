7847c014f8e5543cc017f1d940423f3b9a8e582c
Ana Beatriz Silva Araújo <anabeatrizaraujo0207@gmail.com>
2024-10-29 19:48:09 -0300

    jogo da velha

---
+++ 7 projetos em 7 dias/4 - jogo da velha/script.js
@@ -0,0 +1,82 @@
+// dados iniciais 
+let square = {
+    a1: '',
+    a2: '',
+    a3: '',
+    b1: '',
+    b2: '',
+    b3: '',
+    c1: '',
+    c2: '',
+    c3: ''
+}
+ let player = ''
+ let warning = ''
+ let playing = false
+
+ reset()
+
+// eventos
+document.querySelector('.reset').addEventListener('click', reset)
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick)
+})
+
+
+// funções
+
+
+
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item')
+    if(square[item] === ''){
+        square[item] = player;
+        renderSquare()
+        togglePlayer()
+    }
+}
+
+function reset() {
+    warning = ''
+
+    let random = Math.floor(Math.random() * 2)
+
+    if (random === 0) { 
+        player = 'x'
+    } else {
+        player = 'o'
+    }
+
+    for (let i in square) {
+        square[i] = ''
+
+    }
+
+    playing = true
+
+    renderSquare()
+    renderInfo()
+}
+
+function renderSquare() {
+    for(let i in square) {
+        console.log('ITEM: ', i)
+        let item = document.querySelector(`div[data-item=${i}]`)
+        if(square[i] !== '') {
+            item.innerHTML = square[i]
+        } else {
+            item.innerHTML = '' 
+        }
+    }
+}
+
+function renderInfo() {
+    document.querySelector('.vez').innerHTML = player
+    document.querySelector('.resultado').innerHTML = warning
+}
+
+function togglePlayer() {
+    player = (player === 'x') ? 'o' : 'x'
+    renderInfo()
+}
+
